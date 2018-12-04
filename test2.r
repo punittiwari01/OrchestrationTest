b7a10a59d4e98c1af829c236280891f077a6f5d9
Punit Tiwari <39051765+punittiwari01@users.noreply.github.com>
2018-12-04 18:52:42 +0530

    Update test2.r

--- test2.r
+++ test2.r
@@ -9,7 +9,7 @@ print(PASSWORD)
 print(HOST)
 
 con <- dbConnect(dbDriver("PostgreSQL"),
-                 dbname = "shubhamjupy",
+                 dbname = "peakretail",
                  host = HOST,
                  port = 5439,
                  user = USERNAME,
